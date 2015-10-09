581766579049c39ec3b293f3ca17832f8de9e53d
josuigoa <josuigoa@gmail.com>
2015-10-09 13:52:38 +0200

    little fixes

--- clipboard/Clipboard.hx
+++ clipboard/Clipboard.hx
@@ -6,20 +6,10 @@ package clipboard;
 @:build(linc.Linc.xml('clipboard'))
 extern class Clipboard {
 
-        //external native function definition
-        //can be wrapped in linc::libname or call directly
-        //and the header for the lib included in linc_empty.h
-
     @:native('linc::clipboard::copy')
     static function copy(s:String):Void;
 
     @:native('linc::clipboard::paste')
     static function paste() : String;
-
-        //inline functions can be used as wrappers
-
-    // static inline function example() : Void {
-    //     trace('empty project example');
-    // }
 
 } //Clipboard
--- test/Test.hx
+++ test/Test.hx
@@ -4,9 +4,10 @@ class Test {
         
     static function main() {
 
+    	trace('previously in the clipboard... "${Clipboard.paste()}"');
     	trace('Copying "Hello world" text to clipboard.');
     	Clipboard.copy("Hello world");
-    	trace('this is what is in the clipboard: "${Clipboard.paste()}"');
+    	trace('this is what is in the clipboard now: "${Clipboard.paste()}"');
 
     }
 
